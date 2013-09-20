fa8d6ab76d609c3c2d2ddb83662700a707adaf17
MayRiv <mayriv@2ch.(none)>
2013-09-20 19:17:46 +0300

    added limit in a stack, fixed memory leak when there is now entire frame

--- yuvReciever/stack.c
+++ yuvReciever/stack.c
@@ -9,6 +9,11 @@ int length(Stack *head);
 int addPiece(Stack** head, unsigned char* buf, int size, int IDOfFrame, int pieceNumber, int numberOfPieces, int height, int width, char* camID, char* macAddr)
 {
   pthread_mutex_lock(&mutex);
+  if (length(*head) > 10)
+  {
+    pthread_mutex_unlock(&mutex);
+    return -1;
+  }
   Stack* headp = *head;
 
   Stack* newPiece = (Stack*)malloc(sizeof(Stack));
@@ -49,7 +54,7 @@ int getFrame(Stack** head, unsigned char* frame, int* height, int* width)
   int IDOfFrame         = (*head)->IDOfFrame;
   int totalSize         = 0;
   Stack** arrayToDelete = (Stack**)malloc(sizeof(Stack*) * (headp->numberOfPieces + 1));
-  memset(arrayToDelete, 0, headp->numberOfPieces+1);
+  memset(arrayToDelete, 0, (headp->numberOfPieces+1) * sizeof(Stack*));
   int counter           = 0;
   printf("The length is %d\n", length(*head));
   while( headp != 0)
@@ -58,34 +63,41 @@ int getFrame(Stack** head, unsigned char* frame, int* height, int* width)
     memcpy(frame + shift, headp->buffer, headp->size);
     totalSize      += headp->size;
     shift          += headp->size;
+    arrayToDelete[counter] = headp;
+    counter++;
     if (headp->pieceNumber == headp->numberOfPieces) 
     {
       printf("IDOfFrame is %d\n",headp->IDOfFrame);
       int i=0;
-      for (i = 0; i < headp->numberOfPieces; i++)    //free all the pieces without last one.
+      *head   = headp->next;
+      *height = headp->height;
+      *width  = headp->width;
+
+      for (i = 0; i < headp->numberOfPieces + 1; i++)    
       {
 	printf("freeing %d piece\n",i);
         if (arrayToDelete[i]) free(arrayToDelete[i]->buffer);
         free(arrayToDelete[i]);
       }
       free(arrayToDelete);
-      *height = headp->height;
-      *width  = headp->width;
-      *head   = headp->next;
-      free(headp->buffer);                            // clean memory for last one.
-      free(headp);
       pthread_mutex_unlock(&mutex);
       return totalSize;
     }
     if ((headp->next == 0) || (headp->IDOfFrame != headp->next->IDOfFrame)){ break; }//pthread_mutex_unlock(&mutex); return -1;}
-    arrayToDelete[counter] = headp;
-    counter++;
     headp = headp->next;
     //free(toDelete);
   }
+  int i;
+ // *head = headp->next;
+  /*for (i = 0; i < headp->numberOfPieces + 1; i++)    
+  {
+    printf("freeing %d piece\n",i);
+    if (arrayToDelete[i]) free(arrayToDelete[i]->buffer);
+    free(arrayToDelete[i]);
+  }*/
   free(arrayToDelete);
-  memset(frame, 0, totalSize);
-  printf("There is no entire frame! The last piece is %d\n", headp->pieceNumber);
+  //memset(frame, 0, totalSize);
+  //printf("There is no entire frame! The last piece is %d\n", headp->pieceNumber);
   pthread_mutex_unlock(&mutex);  
   return -1;
 }
